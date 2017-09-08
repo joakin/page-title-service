4d62e8d9ca5e2f63fd5ea5f365cea95f73083a85
joakin <joaquin@chimeces.com>
2017-09-08 18:27:28 +0200

    Load page from argv and print document.title

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
+const url = process.argv.slice(2)[0] || "https://example.com";
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://example.com');
-  await page.screenshot({path: 'example.png'});
+  await page.goto(url, {
+    waitUntil: "networkidle"
+  });
 
+  const title = await page.evaluate(() => document.title);
   browser.close();
+
+  console.log(title);
 })();
