839bd4a2bc7707ff2ef2a3eee1c31abb9bb79521
joakin <joaquin@chimeces.com>
2017-09-08 19:04:44 +0200

    Run API server that takes a url and responds with the title

--- index.js
+++ index.js
@@ -1,15 +1,49 @@
 const puppeteer = require("puppeteer");
+const http = require("http");
 
-const url = process.argv.slice(2)[0] || "https://example.com";
-(async () => {
+const port = process.env.PORT || 3000;
+
+async function main() {
   const browser = await puppeteer.launch();
+
+  process.on("beforeExit", () => {
+    console.log("Closing browser");
+    browser.close();
+  });
+
+  http
+    .createServer(async (req, res) => {
+      const path = req.url.slice(1);
+
+      if (path.startsWith("http")) {
+        console.log(`Requested title of ${path}`);
+        try {
+          const title = await getPageTitle(browser, path);
+          res.statusCode = 200;
+          res.end(title);
+        } catch (err) {
+          res.statusCode = 500;
+          res.end(err.message);
+          console.error(err);
+        }
+      } else {
+        res.statusCode = 404;
+        res.end("Not a valid url");
+      }
+    })
+    .listen(port, () => {
+      console.log(`Server started at ${port}`);
+    });
+}
+
+async function getPageTitle(browser, url) {
   const page = await browser.newPage();
+
   await page.goto(url, {
     waitUntil: "networkidle"
   });
 
-  const title = await page.evaluate(() => document.title);
-  browser.close();
+  return await page.evaluate(() => document.title);
+}
 
-  console.log(title);
-})();
+main();
